079eab41a8f1f09a063afbbce1f02af0b9ba32bd
Ravi Raja Merugu <rrmerugu@gmail.com>
2021-04-20 01:13:18 +0530

    version updated to v0.0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='invana-engine',
-    version='0.0.3',
+    version='0.0.4',
     description='GraphQL API and Insights engine for Apache TinkerPop supported graph databases.',
     author='Ravi Raja Merugu',
     author_email='ravi@invana.io',
