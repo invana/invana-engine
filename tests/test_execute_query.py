7154137c721dabff0e5af2e2288295a1c5916ced
RAVIRAJA-PC\rrmerugu <rrmerugu@gmail.com>
2022-04-04 09:41:42 +0530

    basic test added - wip

--- tests/test_execute_query.py
+++ tests/test_execute_query.py
@@ -3,7 +3,9 @@ from invana import InvanaGraph
 
 def test_execute_query():
     graph = InvanaGraph("ws://localhost:8182/gremlin")
-    response = graph.connector.execute_query("mgmt = graph.openManagement(); mgmt.printSchema()")
+    # response = graph.connector.execute_query("g.addV('person').property('name', 'Rav').next()")
+    response = graph.connector.execute_query("g.V().limit(1).elementMap().toList()")
+    print("===================",response.data)
     assert response.status_code == 200
     assert response.data is not None
     graph.close_connection()
