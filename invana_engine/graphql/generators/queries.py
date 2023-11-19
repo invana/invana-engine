17fee56e6249d6c9a4a1e9c7b0204d4dbc36385e
Ravi Raja Merugu <rrmerugu@gmail.com>
2023-11-20 03:06:27 +0530

    chores - relationship data generators cleanups and args added to type

--- invana_engine/graphql/generators/queries.py
+++ invana_engine/graphql/generators/queries.py
@@ -39,87 +39,73 @@ class QueryGenerators:
         self.schema_defs = schema_defs
         self.schema_types_dict = {"nodes": {}, "relationships": {}}
  
-    def create_where_order_by(self, type_def: InvanaGQLLabelDefinition):
+    def create_where_order_by(self, type_defs: typing.List[InvanaGQLLabelDefinition]):
         # create order by 
         order_by_fields = {}
-        for field_name, field in type_def.fields.items():
-            order_by_fields[field_name] = OrderByEnum()
-        return type(f"{type_def.label}OrderBy", (graphene.InputObjectType, ), order_by_fields)
+        for type_def in type_defs:
+            for field_name, field in type_def.fields.items():
+                order_by_fields[field_name] = OrderByEnum()
+        return type(f"{''.join([type_def.label for type_def in type_defs])}OrderBy", (graphene.InputObjectType, ), order_by_fields)
     
     def create_where_relationship_condition(self, relationship_label, direction, ):
         return type(f"{relationship_label}WhereConditions", (graphene.InputObjectType, ), {
             "id": graphene.String()
         })
 
-    def create_where_conditions(self, type_def: InvanaGQLLabelDefinition):
+    def create_where_conditions(self, type_defs: typing.List[InvanaGQLLabelDefinition]):
         """
         """
         # create where 
         """
         NodeWhereConditions2 is a hack to avoid the error 
         `UnboundLocalError: local variable 'NodeWhereConditions' referenced before assignment`"""
-        NodeWhereConditions2 = type(f"{type_def.label}WhereConditions",(graphene.InputObjectType, ),{})
+        type_defs_label = ''.join([type_def.label for type_def in type_defs])
+        NodeWhereConditions2 = type(f"{type_defs_label}WhereConditions",(graphene.InputObjectType, ),{})
         where_condition_fields = {     
             "_and": NodeWhereConditions2(),
             "_or": NodeWhereConditions2(),
             "_not": NodeWhereConditions2()
         }
         # fields
-        for field_name, field in type_def.fields.items():
-            if list(field.directives.keys()).__len__() >  0:
-                for directive_name, directive_data in field.directives.items():
-                    if directive_name == "relationship":
-                        cls = self.create_where_relationship_condition(
-                                directive_data.node_label,
-                                directive_data.direction,
-                            )
-                        where_condition_fields[f"{directive_data.relationship_label}__{directive_data.node_label}"] = cls()
-            elif field.field_type_str == "String":
-                where_condition_fields[field_name] = StringFilersExpressions()
-            elif field.field_type_str == "Int":
-                where_condition_fields[field_name] = IntFilersExpressions()
+        for type_def in type_defs:
+            for field_name, field in type_def.fields.items():
+                if list(field.directives.keys()).__len__() >  0:
+                    for directive_name, directive_data in field.directives.items():
+                        if directive_name == "relationship":
+                            cls = self.create_where_relationship_condition(
+                                    directive_data.node_label,
+                                    directive_data.direction,
+                                )
+                            where_condition_fields[f"{directive_data.relationship_label}__{directive_data.node_label}"] = cls()
+                elif field.field_type_str == "String":
+                    where_condition_fields[field_name] = StringFilersExpressions()
+                elif field.field_type_str == "Int":
+                    where_condition_fields[field_name] = IntFilersExpressions()
 
         # traversals 
-        return type(f"{type_def.label}WhereConditions",(graphene.InputObjectType,), where_condition_fields)
+        return type(f"{type_defs_label}WhereConditions",(graphene.InputObjectType,), where_condition_fields)
  
     def create_property_field(self, field: InvanaGQLLabelFieldDefinition):
         field_str = field.field_type_str
         return getattr(graphene, field_str)()# TODO - add default etc kwargs from ariadne type object
  
-    def create_relationship_field(self, 
-            type_def_label: str,
-            field_name:str, 
-            relationship_directives: typing.List[InvanaGQLFieldRelationshipDirective]
-        ) ->  graphene.Field:
-        fields = {}
-        args = {}
-
-        if field_name.split("__").__len__() == 3:
-            # individual relation to node map; "_oute__ACTED_IN__Movie"
-
-            # for relationship_directive in relationship_directives:
-            #     fields[relationship_directive.field_name] = self.create_node_type_field_by_name(relationship_directive.node_label)
-            #     fields[f'resolve_{relationship_directive.field_name}'] = resolve_relationship_field_resolver
-            # object_type = type(relationship_directive.node_label, (graphene.ObjectType, ), fields) 
-            return self.create_node_type_field_by_name(relationship_directives[0].node_label)  # TODO - add 
-
-        elif field_name.split("__").__len__() == 2:
-            # individual relation to node map # "_oute__ACTED_IN"
+    def create_relationship_fields_for_grouped_target_nodes(self, 
+                    type_def: InvanaGQLLabelDefinition,
+                    relationships_grouped: typing.Dict[str, typing.List[InvanaGQLFieldRelationshipDirective]]):
+        node_type_fields = {}
+        for field_name, relationship_directives in relationships_grouped.items():
+            fields = {}
+            # TODO - add edge properties
+            target_label_type_defs = [] # relation going to which Node
             for relationship_directive in relationship_directives:
+                if self.schema_defs.nodes[relationship_directive.node_label] not in target_label_type_defs:
+                    target_label_type_defs.append(self.schema_defs.nodes[relationship_directive.node_label])
                 fields[relationship_directive.node_label] = self.create_node_type_field_by_name(relationship_directive.node_label)
-                # fields[f'resolve_{relationship_directive.node_label}'] = resolve_relationship_field_resolver
-            object_type = type(f"{type_def_label}{field_name}", (graphene.ObjectType, ), fields) 
-            return graphene.Field(graphene.List(object_type))  # TODO - add 
-
-        elif field_name in ["_bothe", "_ine", "_oute"]:
-            # add traversal
-            for relationship_directive in relationship_directives:
-                fields[relationship_directive.field_name] = self.create_node_type_field_by_name(relationship_directive.node_label)
-                # fields[f'resolve_{relationship_directive.field_name}'] = resolve_relationship_field_resolver
-            object_type = type(f"{type_def_label}{field_name}", (graphene.ObjectType, ), fields) 
-            return graphene.Field(graphene.List(object_type))  # TODO - add 
-
-        # args =  self.create_node_type_args(type_def)
+                # TODO - add resolver if needed; fields[f'resolve_{relationship_directive.node_label}'] = resolve_relationship_field_resolver
+            object_type = type(f"{type_def.label}{field_name}", (graphene.ObjectType, ), fields) 
+            node_type_fields[field_name] =  graphene.Field(graphene.List(object_type),
+                                        args=self.create_node_type_args(target_label_type_defs))
+        return node_type_fields
 
 
     def create_node_data_type(self, type_def: InvanaGQLLabelDefinition, extra_fields=None):
@@ -133,33 +119,83 @@ class QueryGenerators:
             node_type_fields['label'] = graphene.String()
             node_type_fields[field_name] = self.create_property_field(field)
 
-        # 2. create relationship data fields
-        related_fields = type_def.get_related_data_fields()
-        for field_name, relationship_directives in related_fields.items():
-            node_type_fields[field_name] = self.create_relationship_field(
-                type_def.label,
-                field_name, relationship_directives)
+        # 2. creating relationships fields
+
+        # 2.1. inidividual directions
+        # direction relationship to node ex: "oute__related_to__Node"
+        for field_name, relationship_directives in type_def.directed_relationship_to_node("both").items():
+            target_label = relationship_directives[0].node_label # traverse towards label
+            node_type_fields[field_name] = self.create_node_type_field(self.schema_defs.nodes[target_label])
             
+            
+        # 2.2. relationships grouped by direction and edge label
+        #  ex: "bothe__related_to", 
+        node_type_fields.update(
+            self.create_relationship_fields_for_grouped_target_nodes(
+                type_def,
+                type_def.directed_relationships_grouped_by_edge_label("both")
+            )
+        )
+        #  ex: "oute__related_to", 
+        node_type_fields.update(
+            self.create_relationship_fields_for_grouped_target_nodes(
+                type_def,
+                type_def.directed_relationships_grouped_by_edge_label("out")
+            )
+        )
+        #  ex: "ine__related_to", 
+        node_type_fields.update(
+            self.create_relationship_fields_for_grouped_target_nodes(
+                type_def,
+                type_def.directed_relationships_grouped_by_edge_label("in")
+            )
+        )
+
+        # 2.3. relationships grouped by the direction
+        # ex: "ine"
+        node_type_fields.update(
+            self.create_relationship_fields_for_grouped_target_nodes(
+                type_def,
+                type_def.generic_directed_relationships("in")
+            )
+        )
+
+        # ex: "oute"
+        node_type_fields.update(
+            self.create_relationship_fields_for_grouped_target_nodes(
+                type_def,
+                type_def.generic_directed_relationships("out")
+            )
+        )
+
+        # ex: "bothe"
+        node_type_fields.update(
+            self.create_relationship_fields_for_grouped_target_nodes(
+                type_def,
+                type_def.generic_directed_relationships("both")
+            )
+        )
+        
+        
         if extra_fields:
             node_type_fields.update(extra_fields)
 
         return  type(type_def.label, (graphene.ObjectType, ), node_type_fields)
-        # self.add_type_to_schema_cache(type_def, NodeType) # add to cache 
-        # return NodeType
+ 
 
-    def create_node_type_args(self, type_def: InvanaGQLLabelDefinition, ):
+    def create_node_type_args(self, type_defs: typing.List[InvanaGQLLabelDefinition] ):
         return {
             "limit" : graphene.Argument(graphene.Int, description="limits the result count"),
             "offset" : graphene.Argument(graphene.Int, description="skips x results"),
             "dedup" : graphene.Argument(graphene.Boolean, description="dedups the result"),
-            "order_by" : graphene.Argument(self.create_where_order_by(type_def),
+            "order_by" : graphene.Argument(self.create_where_order_by(type_defs),
                                             description="order the result by"),
-            "where": graphene.Argument(self.create_where_conditions(type_def))
+            "where": graphene.Argument(self.create_where_conditions(type_defs))
         }
     
     def create_node_type_field(self, type_def: InvanaGQLLabelDefinition, extra_args=None ):
         NodeDataType = self.create_node_data_type(type_def)
-        args =  self.create_node_type_args(type_def)
+        args =  self.create_node_type_args([type_def])
         if extra_args:
             args.update(extra_args)
         return graphene.Field(graphene.List(NodeDataType), args=args)
--- invana_engine/graphql/generators/types.py
+++ invana_engine/graphql/generators/types.py
@@ -8,6 +8,9 @@ class InvanaGQLDirections:
     IN = "in"
     BOTH =  "both"    
 
+
+# GraphDirections = "out" | "in" | "both"
+
 @dataclass(frozen=True)
 class InvanaGQLFieldRelationshipDirective:
     node_label: str # return data Node
@@ -36,7 +39,7 @@ class InvanaGQLLabelFieldDefinition:
     def is_data_field(self)-> bool:
         return not self.is_relationship_field()
     
-@dataclass
+@dataclass(frozen=True)
 class InvanaGQLLabelDefinition:
     label: str
     label_type: str
@@ -49,61 +52,136 @@ class InvanaGQLLabelDefinition:
 
     def get_relationship_fields(self)-> typing.Dict[str, InvanaGQLLabelFieldDefinition]:
         return {field_name: field for field_name, field in self.fields.items() if field.is_relationship_field()}
- 
-    def get_fields_grouped_by_relationship_label(self, direction: str="both") -> typing.Dict[str, InvanaGQLFieldRelationshipDirective]:
+    
+    def get_relationship_fields_by_direction(self, direction: str):
         """_summary_
 
         Args:
-            direction (str, optional): _description_. Defaults to "both".
-
-        Returns:
-            {
-                '_bothe__ACTED_IN': [InvanaGQLFieldRelati...'ActedIn'), InvanaGQLFieldRelati...'ActedIn')], 
-                '_bothe__likes': [InvanaGQLFieldRelati...s='Liked')], 
-                '_bothe': [InvanaGQLFieldRelati...'ActedIn'), InvanaGQLFieldRelati...s='Liked'), InvanaGQLFieldRelati...'ActedIn')]
-            }
+            direction (str): _description_
         """
         relationship_fields = self.get_relationship_fields()
         if direction in ["in", "out"]:
-            field_relationships = [field.get_relationship_data() for _, field in relationship_fields.items() \
+            return  [field.get_relationship_data() for _, field in relationship_fields.items() \
                     if field.get_relationship_data().direction == direction]
-        else:
-            field_relationships = [field.get_relationship_data() for _, field in relationship_fields.items()]
+        return [field.get_relationship_data() for _, field in relationship_fields.items()]
 
+ 
+    # def get_fields_grouped_by_relationship_label(self, direction: str="both") -> typing.Dict[str, InvanaGQLFieldRelationshipDirective]:
+    #     """_summary_
+
+    #     Args:
+    #         direction (str, optional): _description_. Defaults to "both".
+
+    #     Returns:
+    #         {
+    #             '_bothe__ACTED_IN': [InvanaGQLFieldRelati...'ActedIn'), InvanaGQLFieldRelati...'ActedIn')], 
+    #             '_bothe__likes': [InvanaGQLFieldRelati...s='Liked')], 
+    #             '_bothe': [InvanaGQLFieldRelati...'ActedIn'), InvanaGQLFieldRelati...s='Liked'), InvanaGQLFieldRelati...'ActedIn')]
+    #         }
+    #     """
+    #     relationship_fields = self.get_relationship_fields()
+    #     if direction in ["in", "out"]:
+    #         field_relationships = [field.get_relationship_data() for _, field in relationship_fields.items() \
+    #                 if field.get_relationship_data().direction == direction]
+    #     else:
+    #         field_relationships = [field.get_relationship_data() for _, field in relationship_fields.items()]
+
+    #     fields_dict = {}
+    #     # 1. grouped by relationship label "_oute__ACTED_IN"
+    #     for field_relationship in field_relationships:
+    #         key = f'_{direction}e__{field_relationship.relationship_label}'
+    #         if fields_dict.get(key) and field_relationship not in fields_dict.get(key):
+    #                 fields_dict[key].append(field_relationship)
+    #         else:
+    #             fields_dict[key] = [field_relationship]
+
+    #     # 2. individual traversal "_oute__ACTED_IN__Movie" and "_oute__ACTED_IN__ShortFilm"
+    #     if direction != "both":
+    #         for relationship in field_relationships:
+    #             # 2. inividual relationship label
+    #             key = f"_{relationship.direction}e__{relationship.relationship_label}__{relationship.node_label}"
+    #             fields_dict[key] = [relationship]
+ 
+    #     # 3. generic in/out/both "_oute"
+    #     all_types = [] 
+    #     for field_name, field  in fields_dict.items():
+    #         all_types.extend(field) 
+    #     all_types=  list(set(all_types))
+    #     if all_types.__len__() > 0:
+    #         fields_dict[f"_{direction}e"] =all_types
+ 
+    #     return fields_dict
+
+    # def get_related_data_fields(self):
+    #     # seperate the in and out relationships
+    #     related_fields = {}
+    #     related_fields.update(self.get_fields_grouped_by_relationship_label(direction="both"))
+    #     related_fields.update(self.get_fields_grouped_by_relationship_label(direction="in"))
+    #     related_fields.update(self.get_fields_grouped_by_relationship_label(direction="out"))
+    #     return related_fields
+
+    def directed_relationship_to_node(self, direction):
+        """
+        Create relationships grouped by relationship label. ex: "out__related_to__Node
+
+
+        Args:
+            direction (_type_): out, in, both
+        """
+        # if direction == "both":
+        #     raise Exception("directed_relationship_to_node direction cannot be 'both', because"
+        #                     "each relationship will have a direction to a Node")
+        field_relationships = self.get_relationship_fields_by_direction(direction)
+            
         fields_dict = {}
-        # 1. grouped by relationship label "_oute__ACTED_IN"
+        for relationship in field_relationships:
+            # 2. inividual relationship label
+            key = f"_{relationship.direction}e__{relationship.relationship_label}__{relationship.node_label}"
+            fields_dict[key] = [relationship]
+        return fields_dict
+         
+
+    def directed_relationships_grouped_by_edge_label(self, direction):
+        """
+        Create relationships grouped by relationship label. ex: "_oute__ACTED_IN"
+
+        Args:
+            direction (_type_): in, out, both
+        """
+        field_relationships = self.get_relationship_fields_by_direction(direction)
+        fields_dict = {}
         for field_relationship in field_relationships:
             key = f'_{direction}e__{field_relationship.relationship_label}'
             if fields_dict.get(key) and field_relationship not in fields_dict.get(key):
-                    fields_dict[key].append(field_relationship)
+                fields_dict[key].append(field_relationship)
             else:
                 fields_dict[key] = [field_relationship]
+        return fields_dict
+    
 
-        # 2. individual traversal "_oute__ACTED_IN__Movie" and "_oute__ACTED_IN__ShortFilm"
-        if direction != "both":
-            for relationship in field_relationships:
-                # 2. inividual relationship label
-                key = f"_{relationship.direction}e__{relationship.relationship_label}__{relationship.node_label}"
-                fields_dict[key] = [relationship]
- 
-        # 3. generic in/out/both "_oute"
+    def generic_directed_relationships(self, direction):
+        """
+        Create relationships grouped by relationship label. ex: "_oute
+
+
+        Args:
+            direction (_type_): out, in, both
+        """
+        fields_dict = self.directed_relationships_grouped_by_edge_label(direction)
+
         all_types = [] 
         for field_name, field  in fields_dict.items():
             all_types.extend(field) 
         all_types=  list(set(all_types))
         if all_types.__len__() > 0:
             fields_dict[f"_{direction}e"] =all_types
- 
         return fields_dict
+         
 
-    def get_related_data_fields(self):
-        # seperate the in and out relationships
-        related_fields = {}
-        related_fields.update(self.get_fields_grouped_by_relationship_label(direction="both"))
-        related_fields.update(self.get_fields_grouped_by_relationship_label(direction="in"))
-        related_fields.update(self.get_fields_grouped_by_relationship_label(direction="out"))
-        return related_fields
- 
+
+
+
+
 
 @dataclass
 class InvanaGQLSchema:
