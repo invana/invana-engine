d41d31df500e58637d5c3e8e18d62b32d8632ad6
Ravi Raja Merugu <rrmerugu@gmail.com>
2024-10-22 01:27:59 +0530

    json importer fixed

--- invana_engine/contribs/importers/json_importer.py
+++ invana_engine/contribs/importers/json_importer.py
@@ -22,15 +22,6 @@ class JSONImporter(ImporterBase):
     links_file_pattern = ["links.json"]
     nodes_map : T.Dict[T.Union[int, str], T.Union[int, str]] = {}
 
-    def __init__(self, base_path: str, invana) -> None:
-        self.base_path = base_path
-        self.invana = invana
-
-        if not os.path.isdir(self.base_path):
-            raise PathNotFound(f"Path '{self.base_path}' not found.")
-        self.nodes_file = os.path.join(self.base_path, "nodes.json")
-        self.edges_file = os.path.join(self.base_path, "links.json")
-
     def read_file(self, file_path):
         with open(file_path, 'r') as file:
             data = json.load(file)
@@ -52,26 +43,3 @@ class JSONImporter(ImporterBase):
                                 **link['properties']
                             )
     
-    def _start(self):
-        g: InvanaTraversalSource = self.invana.backend.g
-        with open(self.nodes_file, 'r') as file:
-            nodes = json.load(file)
-        with open(self.edges_file, 'r') as file:
-            links = json.load(file)
-        
-        nodes_map = {} # nodeid: 
-        
-        for node in nodes:
-            node_instance = g.create_vertex(node['label'], **node['properties']).next()
-            nodes_map[node['id']] = node_instance.id
-        
-        links_instances = []
-        for link in links:
-            links_instance = g.create_edge(node['label'],
-                        nodes_map[link['from']],
-                        nodes_map[link['to']],
-                        **node['properties'])
-            links_instances.append(links_instance)
-        
-        print(f"Created {nodes_map.__len__()} nodes, and {links_instances.__len__()} links")
- 
